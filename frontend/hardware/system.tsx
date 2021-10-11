fa6d67ddb82ac75b0dd4be49a5882f813293febe
Daniel Grondin <king@numsgil.co>
2021-10-11 15:05:52 -0400

    System info is done

--- frontend/hardware/system.tsx
+++ frontend/hardware/system.tsx
@@ -1,8 +1,26 @@
-import {Text} from "@chakra-ui/react";
+import {Spinner, Text} from "@chakra-ui/react";
+import hardware from "systeminformation";
 import {VFC} from "react";
+import useSwr from "swr";
+
+import {fetcher} from "./";
+
+type SystemInfo = {
+	system: hardware.Systeminformation.SystemData;
+	baseboard: hardware.Systeminformation.BaseboardData;
+}
 
 export const System: VFC = () => {
-	return <>
-		<Text>Hello System!</Text>
-	</>;
+	const {data} = useSwr<SystemInfo>("/api/pi", fetcher);
+
+	if (!data) {
+		return <Spinner />;
+	} else {
+		return <>
+			<Text fontWeight="bold">System</Text>
+			<Text>Manufacturer: {data.system.manufacturer}; Model: {data.system.model}; Version: {data.system.version}</Text>
+			<Text fontWeight="bold">Baseboard</Text>
+			<Text>Manufacturer: {data.baseboard.manufacturer}; Model: {data.baseboard.model}; Version: {data.baseboard.version}</Text>
+		</>;
+	}
 };
