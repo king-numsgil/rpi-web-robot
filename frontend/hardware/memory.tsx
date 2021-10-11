96746f44ea876f6c5552f3bfee5ba3765b92f041
Daniel Grondin <king@numsgil.co>
2021-10-11 15:16:26 -0400

    Memory info is done

--- frontend/hardware/memory.tsx
+++ frontend/hardware/memory.tsx
@@ -1,8 +1,27 @@
-import {Text} from "@chakra-ui/react";
+import {Spinner, Text} from "@chakra-ui/react";
+import hardware from "systeminformation";
 import {VFC} from "react";
+import useSwr from "swr";
+
+import {fetcher} from "./";
 
 export const Memory: VFC = () => {
-	return <>
-		<Text>Hello Memory!</Text>
-	</>;
+	const {data} = useSwr<hardware.Systeminformation.MemData>("/api/pi/mem", fetcher, {
+		refreshInterval: 1000,
+	});
+
+	if (!data) {
+		return <Spinner />;
+	} else {
+		return <>
+			<Text fontWeight="bold">System Memory</Text>
+			<Text>Total: {(data.total / 1024 / 1024).toFixed(2)} MiB</Text>
+			<Text>Active: {(data.active / 1024 / 1024).toFixed(2)} MiB</Text>
+			<Text>Available: {(data.available / 1024 / 1024).toFixed(2)} MiB</Text>
+			<Text fontWeight="bold">Swap Memory</Text>
+			<Text>Total: {(data.swaptotal / 1024 / 1024).toFixed(2)} MiB</Text>
+			<Text>Used: {(data.swapused / 1024 / 1024).toFixed(2)} MiB</Text>
+			<Text>Free: {(data.swapfree / 1024 / 1024).toFixed(2)} MiB</Text>
+		</>;
+	}
 };
