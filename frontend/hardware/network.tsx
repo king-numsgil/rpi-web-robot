cd9100cfd8e8b670b9bfc8537fca6ab32ca36d38
Daniel Grondin <king@numsgil.co>
2021-10-11 15:33:36 -0400

    Network is kinda done... does not work on my pc (virtual interface)

--- frontend/hardware/network.tsx
+++ frontend/hardware/network.tsx
@@ -1,8 +1,26 @@
-import {Text} from "@chakra-ui/react";
+import {Spinner, Text} from "@chakra-ui/react";
+import hardware from "systeminformation";
 import {VFC} from "react";
+import useSwr from "swr";
+
+import {fetcher} from "./";
 
 export const Network: VFC = () => {
-	return <>
-		<Text>Hello Network!</Text>
-	</>;
+	const {data} = useSwr<Array<hardware.Systeminformation.NetworkStatsData>>("/api/pi/net", fetcher, {
+		refreshInterval: 1000,
+	});
+
+	if (!data) {
+		return <Spinner />;
+	} else {
+		return <>
+			{data.map(iface => <>
+				<Text fontWeight="bold">{iface.iface}</Text>
+				<Text>operstate: {iface.operstate}</Text>
+				<Text>ms: {iface.ms}</Text>
+				<Text>RX Bytes: {iface.rx_bytes}; Dropped: {iface.rx_dropped}; sec: {iface.rx_sec}</Text>
+				<Text>TX Bytes: {iface.tx_bytes}; Dropped: {iface.tx_dropped}; sec: {iface.tx_sec}</Text>
+			</>)}
+		</>;
+	}
 };
